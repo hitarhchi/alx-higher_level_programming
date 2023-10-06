32952899cd766c56cbcb27b459147d237cdf81b1
Itachi <ayinde_daniel@yahoo.com>
2023-10-05 18:12:05 -0700

    lol

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+
+    if a < b:
+        c = add(a, b)
+        for n in range(90):
+
+
+            if add(c, i)
